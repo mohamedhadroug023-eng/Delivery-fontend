d80e1956b76da5b0e4b9c878b3e38fccd81cc158
mohamedhadroug023-eng <mohamedhadroug023@gmail.com>
2026-09-03 13:42:31 +0100

    Create user_model.dart

---
+++ lib/models/user_model.dart
@@ -0,0 +1,35 @@
+class UserModel {
+  final int id;
+  final String name;
+  final String phone;
+  final String role;
+  final String? token;
+
+  const UserModel({
+    required this.id,
+    required this.name,
+    required this.phone,
+    required this.role,
+    this.token,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'] ?? 0,
+      name: json['name'] ?? '',
+      phone: json['phone'] ?? '',
+      role: json['role'] ?? '',
+      token: json['token'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'phone': phone,
+      'role': role,
+      'token': token,
+    };
+  }
+}

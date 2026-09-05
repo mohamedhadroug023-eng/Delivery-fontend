bf13d09ab441aff36ec8f5f09e5f8612c3ef06c5
mohamedhadroug023-eng <mohamedhadroug023@gmail.com>
2026-09-05 09:15:10 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-
+const errorHandler = require("./middleware/error.middleware");
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");

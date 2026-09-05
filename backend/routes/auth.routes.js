eeb39ed86daecafcf9d967f0e55c1a009958c1c6
mohamedhadroug023-eng <mohamedhadroug023@gmail.com>
2026-09-05 09:38:07 +0100

    Update auth.routes.js

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,19 @@
 const express = require("express");
+
 const { login } = require("../controllers/auth.controller");
+const { authRateLimit } = require("../middleware/rateLimit.middleware");
 
 const router = express.Router();
 
+// =========================================================
+// LOGIN
+// =========================================================
+
 // POST /api/auth/login
-router.post("/login", login);
+router.post(
+  "/login",
+  authRateLimit,
+  login
+);
 
 module.exports = router;
